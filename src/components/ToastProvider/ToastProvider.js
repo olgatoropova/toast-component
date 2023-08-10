eacf5b25214cdce4926da0da8dbb87111c04ba85
Olga Toropova <fluctuacia@gmail.com>
2023-08-10 11:58:19 -0700

    Exercise 5: Keyboard & screen reader (Escape key)

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,8 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
 	const [toasts, setToasts] = React.useState([]);
 
+	useEscapeKey(() => setToasts([]));
+
 	function dismissToast(id) {
 		const newToasts = toasts.filter((toast) => toast.id !== id);
 		setToasts(newToasts);
@@ -21,6 +23,20 @@ function ToastProvider({ children }) {
 			{children}
 		</ToastContext.Provider>
 	);
+}
+
+function useEscapeKey(callback) {
+	function handleKeyDown(e) {
+		if (e.code === 'Escape') {
+			callback();
+		}
+	}
+
+	React.useEffect(() => {
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, []);
 }
 
 export default ToastProvider;
